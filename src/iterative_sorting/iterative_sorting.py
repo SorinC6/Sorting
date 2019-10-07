3c222d22f1926804e83051ee6e80b6e29ed97bc0
Sorin Chis <kish.sorin@yahoo.com>
2019-10-07 15:54:37 +0300

    added bubble sort altgoritm

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,7 +5,6 @@ def selection_sort(arr):
         min_position = i
 
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
         for j in range(i+1, len(arr)):
             if arr[j] < arr[min_position]:
                 min_position = j
@@ -19,12 +18,36 @@ def selection_sort(arr):
 
 
 # TO-DO:  implement the Bubble Sort function below
+# def bubble_sort(arr):
+
+#     for i in range(0, len(arr)-1):
+#         for j in range(0, len(arr)-1-i):
+#             if(arr[j] > arr[j+1]):
+#                 temp = arr[j]
+#                 arr[j] = arr[j+1]
+#                 arr[j+1] = temp
+
+#     return arr
+
+
+# Using While
 def bubble_sort(arr):
+
+    isSorted = True
+    while isSorted:
+        isSorted = False
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                isSorted = True
 
     return arr
 
-
 # STRETCH: implement the Count Sort function below
+
+
 def count_sort(arr, maximum=-1):
 
     return arr
